116e754536b81aa12d2bb88f0856c6ef54707870
Nicolai Gorden <nicolaigorden@gmail.com>
2022-01-15 17:20:08 -0600

    commit tree 6b6029e5860ce5f9d7fc325138cc5c68772bed02

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, driverName) {
+    const newArray = array.filter(driver => driver.toLowerCase() === driverName.toLowerCase());
+    return newArray;
+};
+
+function fuzzyMatch(array, letters) {
+    const fuzzyArray = array.filter(driver => driver.startsWith(letters));
+    return fuzzyArray;
+};
+
+function matchName(array, str) {
+    const exactMatch = array.filter(driver => driver.name === str);
+    return exactMatch;
+};
